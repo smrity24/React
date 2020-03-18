2c219e03e8987039cf6e24b70ff688ec00974614
smrity24 <pparbon7072@gmail.com>
2020-03-17 22:00:56 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Appbar from './components/Appbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Appbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Appbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  
+  
+} from "@material-ui/core";
+
+export default props => (
+  <AppBar position="static">
+    <Toolbar>
+  
+      <Typography variant="headline">News</Typography>
+      
+    </Toolbar>
+  </AppBar>
+);
